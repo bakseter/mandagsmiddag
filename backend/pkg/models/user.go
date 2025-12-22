2dc743646dd810f6681016a06996a62c6925d05f
Emil Aukner <emilaukner@gmail.com>
2025-12-22 11:17:00 +0100

    refactor: update user model
    user model now has relations to different entries in db

--- backend/pkg/models/user.go
+++ backend/pkg/models/user.go
@@ -5,7 +5,15 @@ import (
 )
 
 type User struct {
-	gorm.Model
-	Email string
-	Name  string
+    gorm.Model
+    Email       string `gorm:"uniqueIndex;not null"`
+    Name        string `gorm:"not null"`
+    IsAdmin     bool   `gorm:"default:false"`
+    
+    // Relationships
+    HostedDinners     []Dinner       `gorm:"foreignKey:HostUserID"`
+    AttendedDinners   []Dinner       `gorm:"many2many:dinner_participants;"`
+    Ratings           []Rating       `gorm:"foreignKey:UserID"`
+    PenaltiesReceived []Penalty      `gorm:"foreignKey:UserID"`
+    PenaltiesAssigned []Penalty      `gorm:"foreignKey:AssignedByUserID"`
 }
