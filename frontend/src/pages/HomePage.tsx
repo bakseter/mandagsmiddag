8ac32980a567d776bb581bcc38c3daa546ce2ca2
Emil Aukner <emilaukner@gmail.com>
2026-01-17 14:40:43 +0100

    feat: add dinner api call HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from 'react';
+
+interface Dinner {
+	[key: string]: unknown;
+}
+
 function HomePage() {
-	return <>Hello World</>;
+	const [dinners, setDinners] = useState<Dinner[]>([]);
+
+	async function getDinners() {
+		const res = await fetch(`http://localhost:8080/api/dinners`);
+		const data = await res.json();
+		console.log(data);
+		return data;
+	}
+
+	useEffect(() => {
+		getDinners().then((result) => {
+			setDinners(result);
+		});
+	}, []);
+	return (
+		<div>
+			{dinners.map((dinner, index) => (
+				<div key={index}>{JSON.stringify(dinner)}</div>
+			))}
+		</div>
+	);
 }
 
 export default HomePage;
