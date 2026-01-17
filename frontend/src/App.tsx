b23c5ebb5dc9954833e4bfaf05b083b8f8140dba
Emil Aukner <emilaukner@gmail.com>
2026-01-17 14:08:38 +0100

    fix: remove keycloak refs frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,27 +2,24 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout';
 import ErrorBoundary from './context/ErrorBoundary';
-import { KeycloakProvider } from './context/KeycloakContext';
 import HomePage from './pages/HomePage';
 
 function App() {
 	return (
-		<KeycloakProvider>
-			<BrowserRouter>
-				<ErrorBoundary>
-					<Routes>
-						<Route
-							path='/'
-							element={
-								<Layout>
-									<HomePage />
-								</Layout>
-							}
-						/>
-					</Routes>
-				</ErrorBoundary>
-			</BrowserRouter>
-		</KeycloakProvider>
+		<BrowserRouter>
+			<ErrorBoundary>
+				<Routes>
+					<Route
+						path='/'
+						element={
+							<Layout>
+								<HomePage />
+							</Layout>
+						}
+					/>
+				</Routes>
+			</ErrorBoundary>
+		</BrowserRouter>
 	);
 }
 
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 function HomePage() {
-	return <></>;
+	return <>Hello World</>;
 }
 
 export default HomePage;
