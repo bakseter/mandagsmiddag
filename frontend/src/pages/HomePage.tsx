c307be146747e35837049d3a234cb6b9d39e3ea1
Andreas Salhus Bakseter <6720179+bakseter@users.noreply.github.com>
2026-01-19 23:20:53 +0100

    Add h1 header

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,31 +1,34 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 interface Dinner {
-	[key: string]: unknown;
+    [key: string]: unknown;
 }
 
 function HomePage() {
-	const [dinners, setDinners] = useState<Dinner[]>([]);
+    const [dinners, setDinners] = useState<Dinner[]>([]);
 
-	async function getDinners() {
-		const res = await fetch(`http://localhost:8080/api/dinners`);
-		const data = await res.json();
-		console.log(data);
-		return data;
-	}
+    async function getDinners() {
+        const res = await fetch(`http://localhost:8080/api/dinners`);
+        const data = await res.json();
+        console.log(data);
+        return data;
+    }
 
-	useEffect(() => {
-		getDinners().then((result) => {
-			setDinners(result);
-		});
-	}, []);
-	return (
-		<div>
-			{dinners.map((dinner, index) => (
-				<div key={index}>{JSON.stringify(dinner)}</div>
-			))}
-		</div>
-	);
+    useEffect(() => {
+        getDinners().then((result) => {
+            setDinners(result);
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>MANDAGSMIDDAG</h1>
+
+            {dinners.map((dinner, index) => (
+                <div key={index}>{JSON.stringify(dinner)}</div>
+            ))}
+        </div>
+    );
 }
 
 export default HomePage;
