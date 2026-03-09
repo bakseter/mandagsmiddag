85e652f7660ce45f7899867a29c5f13c20e9849b
Emil Aukner <emilaukner@gmail.com>
2026-03-09 21:40:18 +0100

    fix: update RatingsPage to only fetch current users ratings

--- frontend/src/pages/RatingsPage.tsx
+++ frontend/src/pages/RatingsPage.tsx
@@ -1,16 +1,22 @@
 import RatingCard from '../components/RatingCard';
 import AddRatingForm from '../components/RatingForm';
-import { useGetRatingsQuery } from '../services/rating';
+import { useGetRatingsByUserQuery } from '../services/rating';
+import { useGetCurrentUserQuery } from '../services/user';
 
 const RatingsPage = () => {
-    const { data: ratings = [], isLoading, error } = useGetRatingsQuery();
+    const { data: currentUser } = useGetCurrentUserQuery();
+    const {
+        data: ratings = [],
+        isLoading,
+        error,
+    } = useGetRatingsByUserQuery(currentUser?.id || 0);
 
-    if (isLoading) return <div>Laster vurderinger...</div>;
-    if (error) return <div>Klarte ikke hente vurderinger</div>;
+    if (isLoading) return <div>Laster Ratings...</div>;
+    if (error) return <div>Klarte ikke hente Ratings</div>;
 
     return (
         <div>
-            <h1 className="text-3xl">Alle vurderinger</h1>
+            <h1 className="text-3xl">Alle Ratings</h1>
             <AddRatingForm />
             <div className="my-4">
                 {ratings.map((rating, index) => (
